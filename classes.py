7a7d7c4a7f72445a2c813d22786bf3d24528ed46
Pierig <51481010+PierigS@users.noreply.github.com>
2020-01-30 18:03:22 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,9 +1,18 @@
+import json
+
+
 class Clients:
 
-    def __init__(self, name, first_name, car, road, zip_code, city):
+    def __init__(self, name, first_name, car, road, zip_code, city, id):
         self.name = name
         self.first_name = first_name
         self.car = car
         self.road = road
         self.zip_code = zip_code
         self.city = city
+        self.id = id
+
+    def toJSON(self):
+
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
